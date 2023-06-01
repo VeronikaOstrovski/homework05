fb45b947e2d46676f798ab5c898bd1d386bab9d6
veronikaostrovski <veronika.turkey@gmail.com>
2023-06-01 14:59:39 +0300

    Task 1 done

--- src/Figure.java
+++ src/Figure.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Figure {
+
+public class Figure {
+
+    public Figure() {
+    }
+
+    double height;
+    double width;
+    double length;
+
+    public Figure(double height, double width, double length) {
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+    public int figureSum(double height, double width, double length) {
+        double square = this.height + this.width + this.length;
+        int square1 = (int) square;
+        return square1;
+    }
+
+    public void figureSumFormula() {
+        double formula = 2 * (this.height * this.width + this.height * this.length + this.width * this.length);
+        System.out.println(formula);
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Figure triangle = new Figure();
+        Figure square = new Figure(2, 2, 2);
+        Figure circle = new Figure(4, 4, 4);
+        triangle.figureSumFormula();
+        square.figureSum(2, 2, 2);
+        circle.figureSum(4, 4, 4);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
+
